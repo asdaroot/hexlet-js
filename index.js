469018c68db2a2aeae7ddc1c9c0347fcdd966390
asdaroot <asdaroot@gmail.com>
2023-06-08 23:07:09 +0000

    add prettier

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
+console.log(_.last(['one', 'two']));
 const fn = (a, b) => {
   const c = a + b;
   return c;
 };
+
+fn(1, 3);
